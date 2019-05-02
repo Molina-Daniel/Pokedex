21ebe6ef76379218d893025e8411af490cf40357
Daniel Molina <dmolcap@gmail.com>
2019-05-02 11:20:35 +0200

    pkemons showed in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,57 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-card v-for="(pokemon, index) in pokemons" :key="pokemon.name" class="ma-3">
+      <v-list>
+        <template>
+          <v-subheader :key="pokemon.name">
+            <a :href="pokemon.url">{{ index+1 }}. {{ pokemon.name }}</a>
+          </v-subheader>
+        </template>
+      </v-list>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from "../components/HelloWorld";
 
-  export default {
-    components: {
-      HelloWorld
+export default {
+  data() {
+    return {
+      url: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151",
+      pokemons: []
+    };
+  },
+  methods: {
+    getData() {
+      if (localStorage.pokemons) {
+        this.pokemons = JSON.parse(localStorage.pokemons);
+      } else {
+        fetch(this.url)
+          .then(response => response.json())
+          .then(data => {
+            localStorage.setItem("pokemons", JSON.stringify(data.results));
+            this.pokemons = JSON.parse(localStorage.pokemons);
+          });
+      }
     }
+  },
+  created() {
+    this.getData();
   }
+};
 </script>
+
+<style>
+.v-subheader {
+  display: flex;
+  justify-content: center;
+}
+.v-card {
+  max-width: 500px;
+}
+.v-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fill-height grid-list-md text-xs-center>
+    <v-layout row wrap align-center justify-center>
+      <v-flex class="logo" xs8 offset-xs-2>
+        <v-img
+          max-width="300px"
+          src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png"
+          lazy-src="https://seeklogo.com/images/P/Pokemon-logo-497D61B223-seeklogo.com.png"
+        >
+          <template v-slot:placeholder>
+            <v-layout fill-height align-center justify-center ma-0>
+              <v-progress-circular indeterminate color="blue"></v-progress-circular>
+            </v-layout>
+          </template>
+        </v-img>
+      </v-flex>
+      <v-flex class="pokeball" xs8 offset-xs-2>
+        <!-- <router-link to="/about"> -->
+        <v-img
+          max-width="200px"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+          lazy-src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+        >
+          <template v-slot:placeholder>
+            <v-layout fill-height align-center justify-center ma-0>
+              <v-progress-circular indeterminate color="blue"></v-progress-circular>
+            </v-layout>
+          </template>
+        </v-img>
+        <!-- </router-link> -->
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
